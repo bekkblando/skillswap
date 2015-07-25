03bccd3e08b7ea55fec5ad19ee567a41ad29e3f4
Ubuntu <ubuntu@ip-172-31-16-84.us-west-2.compute.internal>
2015-07-25 05:45:10 +0000

    Commiting

--- skillswap/skillswap/settings.py
+++ skillswap/skillswap/settings.py
@@ -15,7 +15,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, '../staticfiles/')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
--- skillswap/swap/migrations/0001_initial.py
+++ skillswap/swap/migrations/0001_initial.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Skill',
             fields=[
-                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
                 ('name', models.CharField(max_length=140)),
             ],
         ),
