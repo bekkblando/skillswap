738208283f8644c8e5522087ba706237724bb65f
Bekk Blando <bekkblando@gmail.com>
2015-08-03 00:23:58 -0400

    Working on the UI

--- skillswap/swap/forms.py
+++ skillswap/swap/forms.py
@@ -13,4 +13,4 @@ class UserForm(forms.ModelForm):
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ('streetnumber', 'street','city','state','phone')
+        fields = ('streetnumber', 'street','city','zipcode','state','phone')
