18060a9983d87516d10d047949d7dea2ddf39a3d
Bekk Blando <bekkblando@gmail.com>
2015-08-07 01:00:21 -0400

    Register UI

--- skillswap/swap/forms.py
+++ skillswap/swap/forms.py
@@ -7,9 +7,14 @@ class UserForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['username'].label = "* Username:"
-        self.fields['password'].label = "* Password:"
-        self.fields['email'].label = "* Email Address:"
+        self.fields['username'].label = "* Username ↓"
+        self.fields['password'].label = "* Password ↓"
+        self.fields['email'].label = "* Email Address ↓"
+        self.fields['username'].widget.attrs['placeholder'] = "Type Username Here"
+        self.fields['password'].widget.attrs['placeholder'] = "Type Password Here"
+        self.fields['email'].widget.attrs['placeholder'] = "Type Email Here"
+
+
 
 
     class Meta:
@@ -18,6 +23,22 @@ class UserForm(forms.ModelForm):
 
 class ProfileForm(forms.ModelForm):
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['streetaddress'].label = "Street Address ↓"
+        self.fields['city'].label = "City ↓"
+        self.fields['zipcode'].label = "Zipcode ↓"
+        self.fields['phone'].label = "Phone ↓"
+
+
+
+        self.fields['streetaddress'].widget.attrs['placeholder'] = "Type Street Address Here"
+        self.fields['city'].widget.attrs['placeholder'] = "Type City Name Here"
+        self.fields['zipcode'].widget.attrs['placeholder'] = "Type Zipcode Here"
+        self.fields['phone'].widget.attrs['placeholder'] = "Type Phone Number Here"
+
     class Meta:
         model = Profile
         fields = ['streetaddress','city','zipcode','state','phone']
+
+
